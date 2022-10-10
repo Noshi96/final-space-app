2b21f74251354bc325bb09738eabea986b2c9ad5
Noshi96 <2spawel2@gmail.com>
2022-10-11 00:13:14 +0200

    Fix: vercel url api

--- services/external-api/index.js
+++ services/external-api/index.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 const getApiURL = () => {
-  return process.env.NEXT_PUBLIC_FINAL_SPACE_API
+  return (
+    process.env.NEXT_PUBLIC_FINAL_SPACE_API ??
+    'https://finalspaceapi.com/api/v0/'
+  )
 }
 
 const instance = axios.create({
