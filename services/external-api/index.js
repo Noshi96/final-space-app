54216e528900d5c35c29d2ebc2d9b12895751a3a
Noshi96 <2spawel2@gmail.com>
2022-10-11 17:07:42 +0200

    Change env var

--- services/external-api/index.js
+++ services/external-api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const getApiURL = () => {
-  return process.env.NEXT_PUBLIC_FINAL_SPACE_API
+  return process.env.NEXT_PUBLIC_FINAL_SPACE_API_VERCEL
 }
 
 const instance = axios.create({
@@ -11,7 +11,6 @@ const instance = axios.create({
 })
 
 export const getCharacters = async () => {
-  console.log(getApiURL())
   return instance.get('/character')
 }
 
