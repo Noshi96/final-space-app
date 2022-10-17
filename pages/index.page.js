248e330753aa63a699afe8f73c8dbdf2bad127b2
Noshi96 <2spawel2@gmail.com>
2022-10-18 00:44:50 +0200

    Add hero page

--- pages/index.page.js
+++ pages/index.page.js
@@ -1,3 +1,35 @@
+import {
+  Image,
+  SectionHero,
+  Hero,
+  HeroTextBox,
+  HeadingPrimary,
+  HeroDescription,
+  LearnMoreButton
+} from './style'
 export default function Home () {
-  return <></>
+  return (
+    <>
+      <main>
+        <SectionHero>
+          <Hero>
+            <HeroTextBox>
+              <HeadingPrimary>
+                Check out your <span>favorite</span> heroes from series and play{' '}
+                <span>games</span>
+              </HeadingPrimary>
+              <HeroDescription>
+                Learn more about <span>universum</span>
+              </HeroDescription>
+              <LearnMoreButton>Click here</LearnMoreButton>
+            </HeroTextBox>
+            <Image
+              src='https://images5.alphacoders.com/105/1055769.jpg'
+              alt='final-space-wallpaper'
+            />
+          </Hero>
+        </SectionHero>
+      </main>
+    </>
+  )
 }
---
+++ pages/style.js
@@ -0,0 +1,93 @@
+import styled from 'styled-components'
+
+export const SectionHero = styled.section`
+  color: #eee;
+  display: flex;
+  height: 100vh;
+  position: fixed;
+`
+
+export const Hero = styled.div`
+  padding: 0 3.2rem;
+  display: grid;
+  grid-template-columns: 1fr;
+  align-items: center;
+  gap: 9.6rem;
+
+  @media only screen and (min-width: 58rem) {
+    grid-template-columns: 1fr 1fr;
+  }
+`
+
+export const HeroTextBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+`
+
+export const HeadingPrimary = styled.h1`
+  font-size: 2.4rem;
+  margin-bottom: 1.2rem;
+  span {
+    color: #fdac56;
+    font-weight: 600;
+  }
+  @media only screen and (min-width: 48rem) {
+    font-size: 4.4rem;
+    margin-bottom: 2.2rem;
+  }
+`
+export const HeroDescription = styled.p`
+  font-size: 1rem;
+  line-height: 1.2;
+  margin-bottom: 1.2rem;
+
+  span {
+    color: #fdac56;
+    font-weight: 600;
+    font-size: 2.6rem;
+  }
+
+  @media only screen and (min-width: 48rem) {
+    font-size: 2rem;
+    line-height: 1.6;
+    margin-bottom: 2.2rem;
+  }
+`
+
+export const LearnMoreButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0.75rem 1rem;
+  margin-top: 1rem;
+  width: 10rem;
+  height: 2.5rem;
+  background: #7f56d9;
+  border: 1px solid #7f56d9;
+  box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05);
+  border-radius: 24px;
+  color: #fff;
+  cursor: pointer;
+
+  &:hover {
+    background: #7f56a9;
+    border: 1px solid #7f56a9;
+    box-shadow: 0px 5px 2px rgba(16, 24, 40, 0.05);
+  }
+
+  &:active {
+    background: #7f56d9;
+    border: 1px solid #7f56d9;
+    box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05);
+  }
+`
+
+export const Image = styled.img`
+  z-index: -1;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+`
