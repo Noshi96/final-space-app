b82797742c0c02982ae9aa57f0d2d2fe089bf015
Noshi96 <2spawel2@gmail.com>
2023-01-27 20:03:00 +0100

    FSA-11 new imports in characters page

--- src/pages/characters/index.page.js
+++ src/pages/characters/index.page.js
@@ -3,14 +3,14 @@ import {
   getCharacters,
   getEpisodes,
   getFavorites,
-} from '../../services/internal-api';
-import CharacterList from '../../../components/CharacterList/CharacterList';
-import { EpisodesContextProvider } from '../../store/episodes-context';
-import { NewCharactersContextProvider } from '../../store/new-characters-context';
-import SearchBar from '../../../components/SearchBar/SearchBar';
+} from 'services/internal-api';
+import { EpisodesContextProvider } from 'store/episodes-context';
+import { NewCharactersContextProvider } from 'store/new-characters-context';
+import { FavoritesContextProvider } from 'store/favorites-context';
+import CharacterList from 'components/CharacterList/CharacterList';
+import SearchBar from 'components/SearchBar/SearchBar';
+import Layout from 'components/Layout/Layout';
 import { useState } from 'react';
-import Layout from '../../../components/Layout/Layout';
-import { FavoritesContextProvider } from '../../store/favorites-context';
 
 export default function Characters({
   characters,
